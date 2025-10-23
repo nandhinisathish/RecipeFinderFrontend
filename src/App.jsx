a059d97a27baf1ba6f32815cf7c55555e4c10ba3
nandhinisathish <39171961+nandhinisathish@users.noreply.github.com>
2025-10-23 06:35:23 -0400

    Added header and Recipe title

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import './App.css'
+import './App.css';
+
+import Header from "./components/Header.jsx";
+
 
 const App = () => {
-
-
   return (
-    <>
-    
-      
-    </>
-  )
-}
+   
+      <div style={{backgroundColor:"#faf8f8ff"}}>
+        <Header />
+       
+       
+      </div>
 
-export default App
+  );
+};
+
+export default App;
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+
+
+
+const Header = ({}) => {
+  return (
+    <header className="app-header">
+      <h1 className="logo">🍽️ Recipe Finder</h1>
+      <nav className="nav-tabs">
+        <button>Home</button>
+
+        <button>About</button>
+
+        <button>Random Recipe</button>
+
+        <button>Login/ Sign Up</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
